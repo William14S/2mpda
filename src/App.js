0bdbfda65eb79bc5e161f56efe84bad3b114a5ff
William14S <wsandoval7645@richmondhillhs.org>
2021-02-05 18:20:39 +0000

    finalish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Navbar from './Components/Navbar';
+import Dashboard from './Components/Dashobard';
+import Card from './Components/Card';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+     <Navbar/>
+     <Dashboard/>
+     <div className="card-container">
+         <Card category = 'Food' title = 'Pizza'/>
+         <Card category = 'Movie' title = 'Final Destination'/>
+         <Card category = 'Activity' title = 'Existing'/>
+         <Card category = 'Video Game' title = 'GTA or COD'/>
+     </div>
     </div>
   );
 }
